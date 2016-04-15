65f14bf121bbee274913f307f53a04253122494f
lxalxy <lxaljx@163.com>
2016-04-15 21:10:42 +0800

    first commit

--- AD_MTi/Project/A2P_ADC.h
+++
@@ -1,2 +0,0 @@
-void A2P_ADC_Init(void);
-void A2P_ADCDMA_Init(void);
--- AD_MTi/Project/peripherals.h
+++
@@ -1,5 +0,0 @@
-#include<stm32f4xx.h>
-
-void Peripherals_Init(void);
-void Timer3_Init(u16 arr,u16 psc);
-void Timer4_Init(u16 arr,u16 psc);
